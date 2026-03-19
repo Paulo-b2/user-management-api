142a067d044a285a91b994270b3325aa43791d7d
Paulo Eduardo Nunes Ribero <eduardo.paulob2@gmail.com>
2026-03-18 22:07:28 -0300

    feat: adicionar autorização por cargos (RBAC)

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -9,6 +9,7 @@ import { getInactive } from "./list-inactive.js";
 import { authenticate } from "./authenticate.js";
 import { verifyJWT } from "../../middlewares/verify-jwt.js";
 import { refresh } from "./refresh.js";
+import { verifyUserRole } from "../../middlewares/verify-user-role.js";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", create);
@@ -22,7 +23,7 @@ export async function usersRoutes(app: FastifyInstance) {
   app.get("/users/inactive", { onRequest: [verifyJWT] }, getInactive);
 
   app.patch("/users/:id", { onRequest: [verifyJWT] }, update);
-  app.patch("/users/:id/reactivate", { onRequest: [verifyJWT] }, reactivate);
+  app.patch("/users/:id/reactivate", { onRequest: [verifyJWT, verifyUserRole("ADMIN")] }, reactivate);
 
-  app.delete("/users/:id", { onRequest: [verifyJWT] }, deleteUser);
+  app.delete("/users/:id", { onRequest: [verifyJWT, verifyUserRole("ADMIN")] }, deleteUser);
 }
